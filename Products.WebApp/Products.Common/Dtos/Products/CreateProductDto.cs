eaf1e46b0d4470ca6de6afcb17e9ee964cd89a3b
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-15 21:33:56 +0200

    Update the Common Layer.

---
+++ Products.WebApp/Products.Common/Dtos/Products/CreateProductDto.cs
@@ -0,0 +1,24 @@
+﻿using Products.Common.Dtos.Ratings;
+using System.ComponentModel.DataAnnotations;
+
+namespace Products.Common.Dtos.Products
+{
+    public class CreateProductDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public RatingDto Rating { get; set; }
+    }
+}
---
+++ Products.WebApp/Products.Common/Dtos/Ratings/RatingDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Common.Dtos.Ratings
+{
+    public class RatingDto
+    {
+        [Required]
+        public decimal Rate { get; set; }
+        [Required]
+        public int Count { get; set; }
+    }
+}
