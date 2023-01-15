211e34b5403738e53543171524b243ab00f4d2b8
Virginia Varzari <varzarivirginia4@gmail.com>
2023-01-15 20:59:17 +0200

    Add the classes to the Domain Layer.

---
+++ Products.WebApp/Products.Domain/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace Products.Domain
+{
+    public class Rating : BaseEntity
+    {
+        public decimal Rate { get; set; }
+        public int Count { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
